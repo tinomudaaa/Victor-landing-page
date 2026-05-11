0b4d89692f5bab711ce8b5cfe1c17cf0fda3a26d
Tinomuda Saurombe <saurombev@africau.edu>
2026-05-11 03:34:02 +0200

    feat: update API endpoint to absolute URL and refactor CORS middleware for improved header handling

--- server.js
+++ server.js
@@ -46,13 +46,11 @@ async function saveContactMessage(payload) {
 
 app.use(express.json({ limit: '256kb' }));
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(204);
-  }
-  return next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
 });
 app.use(express.static(path.join(__dirname)));
 
